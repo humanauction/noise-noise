ebcdd13a0dc6b9a24881c2f187a47cac612367fa
humanauction <t.a.harris@gmail.com>
2025-09-01 14:39:25 +0100

    Add: noise_machine, accounts: rf URL patterns; Add: guest access, selector URL inclusion.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from .views import CustomSignupView, CustomLoginView, GuestAccessView
+from .views import GuestAccessView
 
-app_name = 'accounts'
 
 urlpatterns = [
-    path('', include('allauth.urls')),
-    path('signup/', CustomSignupView.as_view(), name='signup'),
-    path('login/', CustomLoginView.as_view(), name='login'),
+    
+    # path('login/', CustomLoginView.as_view(), name='login'),
+    # path('signup/', CustomSignupView.as_view(), name='signup'),
     path('guest/', GuestAccessView.as_view(), name='guest_access'),
+    path('', include('allauth.urls')),
 ]
--- noise_machine/urls.py
+++ noise_machine/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('selector.urls')),
+    path('selector/', include('selector.urls')),
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=False), name='home'),
 ]
